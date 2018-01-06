7e6f04c4ee98c9bb5737da115e05f6d42234d7da
Radosław Radomski <rr@vest-fiber.com>
2018-01-06 07:30:48 +0100

    Initial commit, all the logic is working correctly

---
+++ VF Raporty Godzin Pracy/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VF_Raporty_Godzin_Pracy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
