5a533f8a5c6bb8d0421cda5d374105b178498e7a
Radosław Radomski <rr@vest-fiber.com>
2018-01-07 15:09:54 +0100

    Usuniecie zbednego kodu testowego

--- VF Raporty Godzin Pracy/Program.cs
+++ VF Raporty Godzin Pracy/Program.cs
@@ -14,37 +14,7 @@ namespace VF_Raporty_Godzin_Pracy
             var raport = new Raport(arkusz);
             excel.Dispose();
             ZapiszExcel.ZapiszDoExcel(raport);
-            foreach (var pracownik in raport.GetPracownicy())
-            {
-                var nazwaPliku = string.Format(@"d:\test\{0} {1}.txt",pracownik.Nazwisko,pracownik.Imie);
-                var zapisDoPliku = new StreamWriter(nazwaPliku, true);
-                zapisDoPliku.Write("{0} {1} \n",pracownik.Imie, pracownik.Nazwisko );
-                foreach (var dzien in pracownik.GetDni())
-                {
-                    zapisDoPliku.Write("{0} \t",dzien.Date.Date);
-                    foreach (var godzina in dzien.GetGodziny())
-                    {
-                        zapisDoPliku.Write(" {0:F1} \t", godzina);
-                    }
-                    zapisDoPliku.Write("\n");
-                }
-                zapisDoPliku.Write("\n");
-            }
-
-            foreach (var pracownik in raport.GetPracownicy())
-            {
-                plikDoZapisu.Write("{0} {1} \n",pracownik.Imie, pracownik.Nazwisko );
-                foreach (var dzien in pracownik.GetDni())
-                {
-                    plikDoZapisu.Write("{0} \t",dzien.Date.Date);
-                    foreach (var godzina in dzien.GetGodziny())
-                    {
-                        plikDoZapisu.Write(" {0:F1} \t", godzina);
-                    }
-                    plikDoZapisu.Write("\n");
-                }
-                plikDoZapisu.Write("\n");
-            }
+            
         }
     }
 }
