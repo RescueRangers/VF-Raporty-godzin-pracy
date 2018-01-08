c6bf5c7d1336a7468f9053fb393f82771adcbe03
Radosław Radomski <rr@vest-fiber.com>
2018-01-08 11:05:22 +0100

    Nowa metoda konwersja pliku xls do xlsx, Epplus obsluguje tylko xlsx. Usuniecie pozostalosci po konsolowej aplikacji. Dodanie metody tworzenia raportu z podanej sciezki pliku w postaci string.

---
+++ VF Raporty Godzin Pracy/KonwertujPlikExcel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.Office.Interop.Excel;
+namespace VF_Raporty_Godzin_Pracy
+{
+    class KonwertujPlikExcel
+    {
+        public string XlsDoXlsx(string plikExcel)
+        {
+            if (plikExcel == null)
+            {
+                throw new FileNotFoundException("Nie znaleziono pliku");
+            }
+            else if (plikExcel.ToLower()[plikExcel.Count()] == 'x')     
+            {
+                throw new FileFormatException("Niepoprawny typ pliku, oczekiwano .xls");
+            }
+            else
+            {
+                var nazwaPlikuXls = new FileInfo(plikExcel);
+                
+                var plikXls = new Application().Workbooks.Open(plikExcel);
+                plikXls.SaveAs(nazwaPlikuXls.Name.Remove(nazwaPlikuXls.Name.Count() - 4), "xlOpenXMLWorkbook");
+                plikXls.Close(false);
+                return plikExcel + 's';
+            }
+        }
+    }
+}
---
+++ VF Raporty Godzin Pracy/UtworzRaport.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using OfficeOpenXml;
+
+namespace VF_Raporty_Godzin_Pracy
+{
+    public class UtworzRaport
+    {
+        public Raport Stworz(string plikDoRaportu)
+        {
+            var plikExcel = new FileInfo(plikDoRaportu); 
+            if (plikExcel.Extension.ToLower() == ".xls")
+            {
+                throw new FileFormatException("Niepoprawny format pliku, ozekiwano .xlsx");
+            }
+            else if (plikExcel == null)
+            {
+                throw new FileNotFoundException("Nie odnaleziono pliku.");
+            }
+            else
+            {
+                var arkuszExcel = new ExcelPackage(plikExcel).Workbook.Worksheets[1];
+                var raport = new Raport(arkuszExcel);
+                arkuszExcel.Dispose();
+                return raport;
+            }
+            
+        }
+    }
+}
