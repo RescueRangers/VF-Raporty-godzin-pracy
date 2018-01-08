ecaef31e19049934f64e489354c9ea0ee1ce2538
Radosław Radomski <rr@vest-fiber.com>
2018-01-08 14:27:42 +0100

    Podstawowe dzialajace GUI.

--- WinGUI/MainWindow.xaml.cs
+++ WinGUI/MainWindow.xaml.cs
@@ -1,18 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.IO;
 using Microsoft.Win32;
 using VF_Raporty_Godzin_Pracy;
 
@@ -31,36 +19,36 @@ namespace WinGUI
 
         private void Open_Click(object sender, RoutedEventArgs e)
         {
-            try
+            var plikiExcel = "Pliki Excel (*.xls;*.xlsx)|*.xls;*.xlsx";
+            var plikDoRaportu = "";
+            var otworzPlik = new OpenFileDialog
             {
-                var plikiExcel = "Pliki Excel (*.xls;*.xlsx)|*.xls;*.xlsx";
-                var plikDoRaportu = "";
-                var otworzPlik = new OpenFileDialog
-                {
-                    Filter = plikiExcel
-                };
-                if (otworzPlik.ShowDialog() == true)
-                {
-                    plikDoRaportu = otworzPlik.FileName;
-                }
-                if (plikDoRaportu.ToLower()[plikDoRaportu.Count() - 1] == 's')
-                {
-                    plikDoRaportu = KonwertujPlikExcel.XlsDoXlsx(plikDoRaportu);
-                }
-                raport = UtworzRaport.Stworz(plikDoRaportu);
-                Execute.IsEnabled = true;
-            }
-            catch (Exception)
+                Filter = plikiExcel
+            };
+            if (otworzPlik.ShowDialog() == true)
             {
-                MessageBox.Show("Nie udało się otworzyć raportu.");
+                plikDoRaportu = otworzPlik.FileName;
             }
+            if (plikDoRaportu.ToLower()[plikDoRaportu.Count() - 1] == 's')
+            {
+                plikDoRaportu = KonwertujPlikExcel.XlsDoXlsx(plikDoRaportu);
+            }
+            raport = UtworzRaport.Stworz(plikDoRaportu);
+            Execute.IsEnabled = true;
+            JedenPracownik.IsEnabled = true;
+            WyborPracownika.ItemsSource = raport.GetNazwyPracownikow();
         }
 
         private void Execute_Click(object sender, RoutedEventArgs e)
         {
             if (JedenPracownik.IsChecked == true)
             {
-                ZapiszExcel.ZapiszDoExcel(raport, WyborPracownika.SelectedIndex);
+                var wybraniPracownicy = new List<string>();
+                foreach (var item in WyborPracownika.SelectedItems)
+                {
+                    wybraniPracownicy.Add(item.ToString());
+                }
+                ZapiszExcel.ZapiszDoExcel(raport, wybraniPracownicy);
             }
             else
             {
@@ -71,6 +59,11 @@ namespace WinGUI
         private void JedenPracownik_Checked(object sender, RoutedEventArgs e)
         {
             WyborPracownika.IsEnabled = true;
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
