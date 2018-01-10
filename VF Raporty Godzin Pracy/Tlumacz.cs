1aeff4e9ff47c25ce7ad7828625bd5ce186d0a2c
Radosław Radomski <rr@vest-fiber.com>
2018-01-10 08:46:20 +0100

    Dodanie metody ladowania tlumaczen naglowkow z pliku xml.

---
+++ VF Raporty Godzin Pracy/Tlumacz.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml;
+
+namespace VF_Raporty_Godzin_Pracy
+{
+    public static class Tlumacz
+    {
+        public static Dictionary<string, string> LadujTlumaczenia()
+        {
+            var dictionaryTlumaczenia = new Dictionary<string, string>();
+            var tlumaczenia = AppDomain.CurrentDomain.BaseDirectory + $@"\Assets\TlumaczoneNaglowki.xml";
+            var xmlDokument = new XmlDocument();
+            xmlDokument.Load(tlumaczenia);
+            var nodes = xmlDokument.DocumentElement.SelectNodes("/naglowki/naglowek");
+
+            foreach (XmlNode node in nodes)
+            {
+                dictionaryTlumaczenia.Add(node.SelectSingleNode("klucz").InnerText, node.SelectSingleNode("wartosc").InnerText);
+            }
+            return dictionaryTlumaczenia;
+        }
+    }
+}
