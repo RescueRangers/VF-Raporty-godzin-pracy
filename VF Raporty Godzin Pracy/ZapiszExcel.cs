be954f897a2048797cd0309e682e3a2dc54b7584
Radosław Radomski <rr@vest-fiber.com>
2018-01-18 10:55:44 +0100

    Dodanie podsumowania godzin w raporcie

--- VF Raporty Godzin Pracy/ZapiszExcel.cs
+++ VF Raporty Godzin Pracy/ZapiszExcel.cs
@@ -39,39 +39,66 @@ namespace VF_Raporty_Godzin_Pracy
             {
                 var template = $@"{AppDomain.CurrentDomain.BaseDirectory}Assets\template.xlsx";
                 var nazwaPliku = $@"{folderDoZapisu}\{pracownik.NazwaPracownika()}.xlsx";
-
+                var znakiDoWyciecia = new char[2] { ' ', '\n' };
 
                 using (var excel = new ExcelPackage(new FileInfo(template)))
                 {
+                    var dlugoscRaportu = raport.GetNaglowki().Count;
+                    var wysokoscRaportu = pracownik.GetDni().Count;
+
+                    //Nazwa pracownika w komorce A1, pozniej jest merge tej komorki na cala dlugosc raportu
                     excel.Workbook.Worksheets[1].Name = pracownik.NazwaPracownika();
+
                     var arkusz = excel.Workbook.Worksheets[1];
                     arkusz.Cells[1, 1].Value = pracownik.NazwaPracownika();
-                    arkusz.Cells[1, 1, 1, raport.GetNaglowki().Count].Merge = true;
+                    arkusz.Cells[1, 1, 1, dlugoscRaportu + 1].Merge = true;
+
                     var naglowekIndeks = 0;
                     var godziny = 0;
-                    var nadgodziny = 0;
+                    
+
                     arkusz.Cells[2, 1].Value = "Data";
+
+                    //Zapelnianie raportu naglowkami
                     foreach (var naglowek in raport.GetNaglowki())
                     {
                         if (naglowek.Nazwa.ToLower() == "godziny pracy")
                         {
                             godziny = naglowekIndeks +2;
                         }
-                        if (naglowek.Nazwa.ToLower() == "nadgodziny 50%")
+
+                        var tekstNaglowka = "";
+                        var dlugoscTekstu = 0;
+
+                        var slowa = naglowek.Nazwa.Split(' ');
+
+                        //Wstawiam nowe linie jezeli slowo ma 5 lub wiecej liter
+                        foreach (var slowo in slowa)
                         {
-                            nadgodziny = naglowekIndeks +2;
+                            dlugoscTekstu = +slowo.Length + 1;
+                            tekstNaglowka += slowo + " ";
+                            if (dlugoscTekstu >= 5)
+                            {
+                                tekstNaglowka += "\n";
+                                dlugoscTekstu = 0;
+                            }
                         }
 
-                        arkusz.Cells[2, 2 + naglowekIndeks].Value = naglowek.Nazwa;
+
+                        arkusz.Cells[2, 2 + naglowekIndeks].Value = tekstNaglowka.Trim(znakiDoWyciecia);
                         naglowekIndeks++;
                     }
 
                     var dzienIndeks = 0;
+
+                    //Wstawianie dni do pierwszej kolumny
                     foreach (var dzien in pracownik.GetDni())
                     {
                         var godzinaIndeks = 0;
                         arkusz.Cells[3 + dzienIndeks, 1].Value = dzien.Date;
                         arkusz.Cells[3 + dzienIndeks, 1].Style.Numberformat.Format = "dd-mm-yyyy";
+
+                        //Wstawianie godzin do raportu
                         foreach (var godzina in dzien.GetGodziny())
                         {
                             arkusz.Cells[3 + dzienIndeks, 2 + godzinaIndeks].Value = godzina;
@@ -79,12 +106,32 @@ namespace VF_Raporty_Godzin_Pracy
                         }
                         dzienIndeks++;
                     }
+
+                    //Wstawianie dodatkowej kolumny w ktorej beda liczone pprawne godziny
                     arkusz.InsertColumn(godziny+1, 1);
-                    arkusz.Cells[2, godziny].Value = "Godziny\npracy";
-                    for (int i = 3; i < pracownik.GetDni().Count; i++)
+                    arkusz.Cells[2, godziny+1].Value = "Godziny\npracy";
+
+                    arkusz.Cells[2, dlugoscRaportu + 2].Value = "Razem";
+
+                    //Formula liczaca godziny plus podsumowanie
+                    for (int i = 3; i < wysokoscRaportu+3; i++)
                     {
+                        //Godziny
                         arkusz.Cells[i, godziny + 1].FormulaR1C1 = $"round(R{i}C{godziny}-R{i}C{godziny+2},0)";
+                        
+                        //Podsumowanie
+                        arkusz.Cells[i, dlugoscRaportu + 2].FormulaR1C1 = $"sum(R{i}C2:R{i}C{godziny-1})+sum(R{i}C{godziny+1}:R{i}C{dlugoscRaportu+1})";
                     }
+
+                    arkusz.Cells[wysokoscRaportu + 3, 1].Value = "Podsumowanie";
+
+                    for (int i = 2; i < dlugoscRaportu+3; i++)
+                    {
+                        arkusz.Cells[wysokoscRaportu + 3, i].FormulaR1C1 = $"sum(R3C{i}:R{wysokoscRaportu+2}C{i})";
+                    }
+
+                    arkusz.Cells.AutoFitColumns();
+                    arkusz.Cells[2, 1, 2, dlugoscRaportu + 3].Style.WrapText = true;
                     arkusz.Column(godziny).Hidden = true;
 
                     excel.SaveAs(new FileInfo(nazwaPliku));
